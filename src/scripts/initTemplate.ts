62109ba58a018400c59108a62f24aa5d66053430
Nurbek <nurbek-2395@mail.ru>
2021-10-24 10:42:36 +0600

    move prisma folder

--- src/scripts/initTemplate.ts
+++ src/scripts/initTemplate.ts
@@ -1,10 +1,9 @@
 import path from "path";
 import fs from "fs-extra";
-import { generateOutFolder, copyPrismaIntoOutFolder } from "../utils";
+import { generateOutFolder } from "../utils";
 
 export function initTemplate(outFolderPath: string) {
   if (!fs.existsSync(outFolderPath) || path.resolve(outFolderPath) === process.cwd()) {
     generateOutFolder(outFolderPath);
-    copyPrismaIntoOutFolder(outFolderPath);
   }
 }
